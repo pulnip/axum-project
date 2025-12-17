f9291a2c13cd12be9dfdcd0607a1c71e0f3f84af
pulnip <choiwh1008@gmail.com>
2025-12-18 00:17:15 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## axum-project
+
+from 『LUVIT 실전 백엔드 러스트 Axum 프로그래밍』
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use axum::{
+    routing::{delete, get, post, put},
+    Router
+};
+
+fn main() {
+    println!("Hello, world!");
+}
