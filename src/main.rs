085dd8cd896b139f8781371e088f8400b61d839a
pulnip <choiwh1008@gmail.com>
2025-12-18 05:29:08 +0900

    make new router

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use axum::{
     Router
 };
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(|| async move { "Welcome to Axum!" }))
+        .route("/", post(|| async move { "Post Something!" }))
+        .route("/", put(|| async move { "Updating..." }))
+        .route("/", delete(|| async move { "Deleting" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
